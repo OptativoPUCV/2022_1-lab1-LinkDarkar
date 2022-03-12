5a04f67f97c69b1a65d72c7adc533b75374bc525
Your Name <you@example.com>
2022-03-12 15:16:12 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -11,22 +11,25 @@ Función que recibe 3 parámetros (a, b y c),
 y en c almacena el valor de la suma de a más b.
 */
 
-void suma(int a, int b, int * c) {
-    
+void suma(int a, int b, int * c)
+{
+   *c = a + b;
 }
 
 /*
 Esta función suma los N primeros números del arreglo a
 */
-int sumaN(int a[], int N) {
-    int i;
-    int tot = 0;
+int sumaN(int a[], int N)
+{
+   int i;
+   int tot = 0;
 
-    for(i = 0; i < N; i++) {
+   for(i = 0; i < N; i++)
+   {
       tot += a[i];
-    }
+   }
 
-    return tot;
+   return tot;
 }
 
 /*
@@ -36,7 +39,8 @@ arreglo a y almacena el resultado en *suma.
 Utilice la función sumaN.
 */
 
-void sumaNultimos(int a[], int n, int m, int * suma) {
+void sumaNultimos(int a[], int n, int m, int * suma)
+{
 
 }
 
@@ -47,14 +51,16 @@ crearPersona(...), la cual crea una nueva persona con
 los datos correspondientes y retorna un puntero al dato 
 recién creado.
 */
-typedef struct {
+typedef struct
+{
   char nombre[30];
   char rut[11];
   int edad;
 } Persona;
 
 
-Persona* crearPersona(char nombre[], char rut[], int edad) {
+Persona* crearPersona(char nombre[], char rut[], int edad)
+{
    return NULL;
 }
 
@@ -66,12 +72,14 @@ un vector con capacidad `n`, reserva la memoria
 correspondiente para el arreglo con `n` datos 
 inicializados en 0 y luego retorna el vector creado.
 */
-typedef struct {
+typedef struct
+{
    int * datos; // arreglo dinámico
    int capacidad; // capacidad del arreglo
 } Vector;
 
-Vector * crearVector(int n) {
+Vector * crearVector(int n)
+{
    return NULL;
 }
 
@@ -80,7 +88,8 @@ Ejercicio 5a.
 Programe la función void asignarValor(Vector * v, int i, int valor), 
 la cual asigna el valor a la posición i del vector v.
 */
-void asignarValor(Vector * v, int i, int valor) {
+void asignarValor(Vector * v, int i, int valor)
+{
 
 }
 
@@ -89,7 +98,8 @@ Ejercicio 6.
 Programe la función int obtenerValor(Vector * v, int i), 
 la cual retorna el valor en la posición i del vector v.
 */
-int obtenerValor(Vector * v, int i) {
+int obtenerValor(Vector * v, int i)
+{
    return 0;
 }
 
@@ -98,7 +108,8 @@ Ejercicio 7.
 Función que suma los vectores `a` y `b` y 
 actualiza el vector `c` con el resultado de la suma.
 */
-void sumaV(Vector * a, Vector * b, Vector * c) {
+void sumaV(Vector * a, Vector * b, Vector * c)
+{
 
 }
 
@@ -107,6 +118,7 @@ Ejercicio 8.
 Use las operaciones implementadas de vectores para 
 sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 */
-void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
+void sumaV2(int a1, int a2, int b1, int b2, Vector *c)
+{
 
 }
